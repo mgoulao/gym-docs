e50e782c5630b186c971bbb8a02b3e993f3def0f
Manuel Goulão <msilvagoulao@gmail.com>
2022-03-01 16:23:13 +0000

    Deploy website

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BUILD_PATH=docs/build/html
-
-touch $BUILD_PATH/.nojekyll && git add $BUILD_PATH && git commit -m \"Deploy website to gh-pages\" && git subtree push --prefix $BUILD_PATH origin gh-pages
