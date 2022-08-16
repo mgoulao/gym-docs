9fed3d4a417cc6a81996beff4025278b804b806c
mgoulao <mgoulao@users.noreply.github.com>
2022-08-16 14:02:47 +0000

    deploy: 4fa8f249deb5caf6a7814bce2f4257d20aeaec3a

--- scripts/inject_version_menu.py
+++ scripts/inject_version_menu.py
@@ -14,17 +14,15 @@ class CustomHTMLParser(HTMLParser):
         self.old_menu_lines = []
         self.body_end_line = 0
 
-
     def handle_starttag(self, tag, attrs):
         if tag == "div":
             if self.parsing_menu:
                 self.open_divs += 1
-        
+
             for attr in attrs:
                 if attr[0] == "class" and attr[1] == "versions_menu":
                     self.parsing_menu = True
                     self.old_menu_lines.append(self.getpos()[0])
-
 
     def handle_endtag(self, tag):
         if tag == "div":
@@ -37,7 +35,7 @@ class CustomHTMLParser(HTMLParser):
 
 
 def inject_menu(path):
-    n_content = ''
+    n_content = ""
     with open(path, "r+") as fp:
         content = fp.read()
         parser = CustomHTMLParser()
@@ -45,10 +43,18 @@ def inject_menu(path):
 
         arr_content = content.split("\n")
         if len(parser.old_menu_lines) == 0:
-            arr_content = arr_content[:parser.body_end_line-1] + [version_menu_html] + arr_content[parser.body_end_line-1:]
+            arr_content = (
+                arr_content[: parser.body_end_line - 1]
+                + [version_menu_html]
+                + arr_content[parser.body_end_line - 1 :]
+            )
         else:
-            arr_content = arr_content[:parser.old_menu_lines[0]-1] + [version_menu_html] + arr_content[parser.old_menu_lines[1]:]
-        n_content = '\n'.join(arr_content)
+            arr_content = (
+                arr_content[: parser.old_menu_lines[0] - 1]
+                + [version_menu_html]
+                + arr_content[parser.old_menu_lines[1] :]
+            )
+        n_content = "\n".join(arr_content)
 
         fp.seek(0)
         fp.truncate()
@@ -71,7 +77,10 @@ def recursive_injection(path):
 if __name__ == "__main__":
     base_path = sys.argv[1]
     assert not base_path.strip() == "", "Provide a base path"
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"versions_menu.html"), "r") as fp:
+    with open(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), "versions_menu.html"),
+        "r",
+    ) as fp:
         version_menu_html = fp.read()
 
     recursive_injection(base_path)
