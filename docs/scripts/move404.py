1cbb258578241d79ddb48b571516ba9587b9766f
Manuel Goulão <msilvagoulao@gmail.com>
2022-08-18 15:33:34 +0100

    mssing

--- docs/scripts/move404.py
+++ docs/scripts/move404.py
@@ -11,3 +11,5 @@ if __name__ == "__main__":
         content = content.replace('href="../', 'href="/').replace('src="../', 'src="/')
         fp.seek(0)
         fp.truncate()
+
+        fp.write(content)
