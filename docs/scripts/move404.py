fae03dff57af15328f75134223d42158c7b5bec2
Manuel Goulão <msilvagoulao@gmail.com>
2022-08-18 15:29:31 +0100

    add a script to update 404 links

---
+++ docs/scripts/move404.py
@@ -0,0 +1,13 @@
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Provide a path")
+    filePath = sys.argv[1]
+
+    with open(filePath, "w+") as fp:
+        content = fp.read()
+        content = content.replace('href="../', 'href="/').replace('src="../', 'src="/')
+        fp.seek(0)
+        fp.truncate()
